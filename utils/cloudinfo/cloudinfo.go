eec9cdcc2d1c231fdb2c4370f9ee2758bf472606
Christian Stewart <christian@paral.in>
2019-03-03 04:00:58 -0800

    cloudprovider_thirdparty: add build tag
    Signed-off-by: Christian Stewart <christian@paral.in>

--- utils/cloudinfo/cloudinfo.go
+++ utils/cloudinfo/cloudinfo.go
@@ -55,48 +55,6 @@ func (self *realCloudInfo) GetInstanceID() info.InstanceID {
 	return self.instanceID
 }
 
-func detectCloudProvider() info.CloudProvider {
-	switch {
-	case onGCE():
-		return info.GCE
-	case onAWS():
-		return info.AWS
-	case onAzure():
-		return info.Azure
-	case onBaremetal():
-		return info.Baremetal
-	}
-	return info.UnknownProvider
-}
-
-func detectInstanceType(cloudProvider info.CloudProvider) info.InstanceType {
-	switch cloudProvider {
-	case info.GCE:
-		return getGceInstanceType()
-	case info.AWS:
-		return getAwsInstanceType()
-	case info.Azure:
-		return getAzureInstanceType()
-	case info.Baremetal:
-		return info.NoInstance
-	}
-	return info.UnknownInstance
-}
-
-func detectInstanceID(cloudProvider info.CloudProvider) info.InstanceID {
-	switch cloudProvider {
-	case info.GCE:
-		return getGceInstanceID()
-	case info.AWS:
-		return getAwsInstanceID()
-	case info.Azure:
-		return getAzureInstanceID()
-	case info.Baremetal:
-		return info.UnNamedInstance
-	}
-	return info.UnNamedInstance
-}
-
 // TODO: Implement method.
 func onBaremetal() bool {
 	return false
